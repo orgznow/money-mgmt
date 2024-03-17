34ba7151e2a27ab1f38524667955586420d7e53e
neil-o-wilson <neil.os.wilson@gmail.com>
2024-03-17 14:02:40 -0400

    Refactor javascript code
    - Refactor javascript code

--- src/main/resources/static/js/all-entries-mgr.js
+++ src/main/resources/static/js/all-entries-mgr.js
@@ -3,62 +3,33 @@ window.onload = function() {
 }
 
 function onItemBaseAmountInFocus(element) {
-    const elementCompoundId = element.id.split(".");
-    let rowId = elementCompoundId[0];
-
-    let rowRateEl = document.getElementById(rowId + ".rateAmount");
-    let rowRateAmount = parseFloat(rowRateEl.value);
-
-    let rowQuantityEl = document.getElementById(rowId + ".quantity");
-    let rowQuantityAmount = parseFloat(rowQuantityEl.value);
-
+    let rowId = element.id.split(".")[0];
+    let rowRateAmount = parseFloat(document.getElementById(rowId + ".rateAmount").value);
+    let rowQuantityAmount = parseFloat(document.getElementById(rowId + ".quantity").value);
     element.value = rowRateAmount * rowQuantityAmount;
 }
 
 function onItemTaxAmountInFocus(element) {
-    const elementCompoundId = element.id.split(".");
-    let rowId = elementCompoundId[0];
-
-    let rowIsTaxableEl = document.getElementById(rowId + ".isTaxable1");
-    let rowIsTaxableChecked = rowIsTaxableEl.checked;
-
-    let rowBaseAmountEl = document.getElementById(rowId + ".baseAmount");
-    let rowBaseAmount = parseFloat(rowBaseAmountEl.value);
-
-    let rowDiscountEl = document.getElementById(rowId + ".discountAmount");
-    let rowDiscountAmount = parseFloat(rowDiscountEl.value);
-
+    let rowId = element.id.split(".")[0];
+    let rowBaseAmount = parseFloat(document.getElementById(rowId + ".baseAmount").value);
+    let rowDiscountAmount = parseFloat(document.getElementById(rowId + ".discountAmount").value);
     let rowWithDiscountAmount = isNaN(rowDiscountAmount) ? rowBaseAmount : rowBaseAmount - rowDiscountAmount;
-
+    let rowIsTaxableChecked = document.getElementById(rowId + ".isTaxable1").checked;
     let taxAmount = (rowIsTaxableChecked) ? rowWithDiscountAmount * 0.075 : 0.0;
-
     if (taxAmount != 0.0) {
-        element.value = taxAmount;
+        element.value = taxAmount.toFixed(2);
     }
 }
 
 function onItemFinalAmountInFocus(element) {
-    const elementCompoundId = element.id.split(".");
-    let rowId = elementCompoundId[0];
-
-    let rowBaseAmountEl = document.getElementById(rowId + ".baseAmount");
-    let rowBaseAmount = parseFloat(rowBaseAmountEl.value);
-
-    let rowDiscountEl = document.getElementById(rowId + ".discountAmount");
-    let rowDiscountAmount = parseFloat(rowDiscountEl.value);
-
+    let rowId = element.id.split(".")[0];
+    let rowBaseAmount = parseFloat(document.getElementById(rowId + ".baseAmount").value);
+    let rowDiscountAmount = parseFloat(document.getElementById(rowId + ".discountAmount").value);
     let rowWithDiscountAmount = isNaN(rowDiscountAmount) ? rowBaseAmount : rowBaseAmount - rowDiscountAmount;
-
-    let rowIsTaxableEl = document.getElementById(rowId + ".isTaxable1");
-    let rowIsTaxableChecked = rowIsTaxableEl.checked;
-
-    let taxAmount = (rowIsTaxableChecked) ? rowWithDiscountAmount * 0.075 : 0.0;
-
-    let rowTipEl = document.getElementById(rowId + ".tipAmount");
-    let rowTipAmount = parseFloat(rowTipEl.value);
-
-    let finalAmount = rowWithDiscountAmount + taxAmount + (isNaN(rowTipAmount) ? 0.0 : rowTipAmount);
-
+    let rowIsTaxableChecked = document.getElementById(rowId + ".isTaxable1").checked;
+    let taxAmount = (rowIsTaxableChecked) ? parseFloat(document.getElementById(rowId + ".taxAmount").value) : 0.0;
+    let rowTipAmount = parseFloat(document.getElementById(rowId + ".tipAmount").value);
+    let finalAmount = rowWithDiscountAmount + (isNaN(taxAmount) ? 0.0 : taxAmount) + (isNaN(rowTipAmount) ? 0.0 : rowTipAmount);
     element.value = finalAmount;
 }
 
