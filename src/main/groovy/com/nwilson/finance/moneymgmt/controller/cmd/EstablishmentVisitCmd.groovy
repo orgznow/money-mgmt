3cd4bb0127603b90a4953f39973dd1feb005329b
neil-o-wilson <neil.os.wilson@gmail.com>
2024-03-20 22:42:36 -0400

    Whitespace
    Whitespace

--- src/main/groovy/com/nwilson/finance/moneymgmt/controller/cmd/EstablishmentVisitCmd.groovy
+++ src/main/groovy/com/nwilson/finance/moneymgmt/controller/cmd/EstablishmentVisitCmd.groovy
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 import org.springframework.format.annotation.DateTimeFormat
 
-
 @ToString(includeNames=true)
 class EstablishmentVisitCmd {
 
