f7bc11875ec5faeeff540861f77a3e85986cf870
neil-o-wilson <neil.os.wilson@gmail.com>
2024-03-23 18:36:40 -0400

    Move everything but onCategoryLoseFocus() to jquery

--- src/main/resources/static/js/jqr-all-entries-mgr.js
+++ src/main/resources/static/js/jqr-all-entries-mgr.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     setFocusToAppropriateElement();
+    setOnItemDescriptionsInFocusHandler();
+    setOnItemRateAmountsInFocusHandler();
+    setOnItemBaseAmountsInFocusHandler();
+    setOnItemTaxAmountsInFocusHandler();
+    setOnItemFinalAmountsInFocusHandler();
+    setOnItemFinalAmountsLoseFocusHandler();
+    //setOnCategoryLoseFocusHandler();
 });
 
 function setFocusToAppropriateElement() {
@@ -7,15 +14,97 @@ function setFocusToAppropriateElement() {
     if (visitTotalAmt === "") {
          $("#visitTotalAmount").focus();
     } else {
-        $(".journalEntriesRow:last > td:nth-of-type(2) > input").focus();
+        $(".journalEntriesRows:last > td:nth-of-type(2) > input").focus();
     }
 }
 
-function onItemFinalAmountOutFocus(element) {
+function setOnItemDescriptionsInFocusHandler() {
+    $(".journalEntriesDescription").focus(onItemDescriptionInFocus);
+}
+
+function onItemDescriptionInFocus() {
+    //TODO: replace with count of rows being 1
+    let elementRowIndex = Number($(this).attr("name").split("[").pop().split("]")[0]);
+    if (elementRowIndex === 0) {
+        let description = $("#visitDescription").val();
+        $(this).val(description);
+    }
+}
+
+function setOnItemRateAmountsInFocusHandler() {
+    $(".journalEntriesRate").focus(onItemRateAmountInFocus);
+}
+
+function onItemRateAmountInFocus() {
+    //TODO: replace with count of rows being 1
+    let elementRowIndex = Number($(this).attr("name").split("[").pop().split("]")[0]);
+    if (elementRowIndex === 0) {
+        let visitTotalAmount = parseFloat($("#visitTotalAmount").val());
+        $(this).val(visitTotalAmount);
+    }
+}
+
+function setOnItemBaseAmountsInFocusHandler() {
+    $(".journalEntriesBaseAmount").focus(onItemBaseAmountInFocus);
+}
+
+function onItemBaseAmountInFocus() {
+    let rowRateAmount = parseFloat($(this).closest("tr").find(".journalEntriesRate").val());
+    let rowQuantityAmount = parseFloat($(this).closest("tr").find(".journalEntriesQuantity").val());
+    $(this).val(rowRateAmount*rowQuantityAmount);
+}
+
+function setOnItemTaxAmountsInFocusHandler() {
+    $(".journalEntriesTaxAmount").focus(onItemTaxAmountInFocus);
+}
+
+function onItemTaxAmountInFocus() {
+    let rowBaseAmount = parseFloat($(this).closest("tr").find(".journalEntriesBaseAmount").val());
+    let rowDiscountAmount = parseFloat($(this).closest("tr").find(".journalEntriesDiscountAmount").val());;
+    let rowWithDiscountAmount = isNaN(rowDiscountAmount) ? rowBaseAmount : rowBaseAmount - rowDiscountAmount;
+    let rowIsTaxableChecked = $(this).closest("tr").find(".journalEntriesIsTaxable").is(":checked");
+    let taxAmount = (rowIsTaxableChecked) ? rowWithDiscountAmount * 0.075 : 0.0;
+    if (taxAmount != 0.0) {
+        $(this).val(taxAmount.toFixed(2));
+    } else {
+        $(this).val("");
+    }
+}
+
+function setOnItemFinalAmountsInFocusHandler() {
+    $(".journalEntriesFinalAmount").focus(onItemFinalAmountInFocus);
+}
+
+function onItemFinalAmountInFocus() {
+    let rowBaseAmount = parseFloat($(this).closest("tr").find(".journalEntriesBaseAmount").val());
+    let rowDiscountAmount = parseFloat($(this).closest("tr").find(".journalEntriesDiscountAmount").val());;
+    let rowWithDiscountAmount = isNaN(rowDiscountAmount) ? rowBaseAmount : rowBaseAmount - rowDiscountAmount;
+    let rowIsTaxableChecked = $(this).closest("tr").find(".journalEntriesIsTaxable").is(":checked");
+    let taxAmount = (rowIsTaxableChecked) ? rowWithDiscountAmount * 0.075 : 0.0;
+    let rowTipAmount = parseFloat($(this).closest("tr").find(".journalEntriesTipAmount").val());
+    let finalAmount = rowWithDiscountAmount + (isNaN(taxAmount) ? 0.0 : taxAmount) + (isNaN(rowTipAmount) ? 0.0 : rowTipAmount);
+    $(this).val(finalAmount);
+}
+
+function setOnItemFinalAmountsLoseFocusHandler() {
+    $(".journalEntriesFinalAmount").focusout(onItemFinalAmountLoseFocus);
+}
+
+function onItemFinalAmountLoseFocus(element) {
       var grandTotal = 0.0;
       $(".journalEntriesFinalAmount").each(function(){
         let rowFinalAmount = parseFloat($(this).val());
         grandTotal = grandTotal + rowFinalAmount;
       });
-      $("#grandTotal").text(grandTotal);
+      $("#visitGrandTotal").text(grandTotal);
 }
+
+//function setOnCategoryLoseFocusHandler() {
+//    $(".journalEntriesSpendCategory").focus(onCategoryLoseFocus);
+//}
+//
+//function onCategoryLoseFocus() {
+//    window.setTimeout(function() {
+//        $("#addItemBtn").focus({ focusVisible: true });
+//    }, 0);
+//}
